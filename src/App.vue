148fbf2112becb0c2f13d95d520a04c8de7801c7
Le Minh Tri <ansidev@gmail.com>
2020-10-23 02:46:24 +0700

    Reproduce issue

--- src/App.vue
+++ src/App.vue
@@ -27,49 +27,6 @@
               media: '(min-width: 1200px)'
             }
           ]
-        },
-        {
-          src: 'https://picsum.photos/2000/1200/?image=10',
-          alt: 'Another alt text',
-          thumbnail: 'https://picsum.photos/64/64/?image=10',
-          caption: 'Another Caption'
-        },
-        {
-          src: 'https://picsum.photos/400/600/?image=20',
-          largeViewSrc: 'https://picsum.photos/2000/2000/?image=20',
-          thumbnail: 'https://picsum.photos/64/64/?image=20'
-        },
-        {
-          src: 'https://picsum.photos/600/400/?image=30',
-          thumbnail: 'https://picsum.photos/64/64/?image=30'
-        },
-        {
-          src: 'https://picsum.photos/600/400/?image=40',
-          thumbnail: 'https://picsum.photos/64/64/?image=40'
-        },
-        {
-          src: 'https://picsum.photos/600/400/?image=50',
-          thumbnail: 'https://picsum.photos/64/64/?image=50'
-        },
-        {
-          src: 'https://picsum.photos/400/600/?image=60',
-          thumbnail: 'https://picsum.photos/64/64/?image=60'
-        },
-        {
-          src: 'https://picsum.photos/600/400/?image=70',
-          thumbnail: 'https://picsum.photos/64/64/?image=70'
-        },
-        {
-          src: 'https://picsum.photos/600/400/?image=80',
-          thumbnail: 'https://picsum.photos/64/64/?image=80'
-        },
-        {
-          src: 'https://picsum.photos/400/700/?image=90',
-          thumbnail: 'https://picsum.photos/64/64/?image=90'
-        },
-        {
-          src: 'https://picsum.photos/2000/1500/?image=100',
-          thumbnail: 'https://picsum.photos/64/64/?image=100'
         }
       ]"
     />
